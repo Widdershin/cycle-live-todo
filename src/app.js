b9110c1fb4fcee02e4d9be39a0f7830fcbe6e9e4
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-18 22:08:58 +1300

    Display todos

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
 import {Rx} from '@cycle/core';
 import {h} from '@cycle/dom';
 
+function todoView ({todo, complete}) {
+  return (
+    h('.todo', [
+      h('span.title', todo),
+      h('input.complete', {type: 'checkbox', checked: complete})
+    ])
+  );
+}
+
+function todosView (todos) {
+  return (
+    h('.todos', todos.map(todoView))
+  );
+}
+
 export default function main ({DOM}) {
+  const todos$ = Rx.Observable.just([
+    {todo: 'Display todos', complete: true},
+    {todo: 'Add todos', complete: false}
+  ]);
+
   return {
-    DOM: Rx.Observable.just(h('.hi', 'Hello world'))
+    DOM: todos$.map(todosView)
   };
 }
 
