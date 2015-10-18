f112b181e0bc55cfb6b29b1d95ee11651c01f658
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-18 22:38:21 +1300

    Use LiveReload instead of browserify-hmr because of cycle-time-travel

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 import budo from 'budo';
 import babelify from 'babelify';
-import hotModuleReloading from 'browserify-hmr';
 
 budo('./index.js', {
-  live: '*.{css,html}',
+  live: true,
   port: 8002,
   stream: process.stdout,
   browserify: {
-    transform: babelify,
-    plugin: hotModuleReloading
+    transform: babelify
   }
 });
